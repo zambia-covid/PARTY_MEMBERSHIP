5c89e6190fa99296aaa40c4e32f86075d185b776
ZAMBIA1 <63206519+zambia-covid@users.noreply.github.com>
2026-04-19 11:54:01 +0200

    Update db.py

--- db.py
+++ db.py
@@ -4,12 +4,12 @@ import psycopg2
 def get_db():
     db_url = os.getenv("DATABASE_URL")
 
-    if not db_url:
-        return psycopg2.connect(
-            host="localhost",
-            database="membership_db",
-            user="postgres",
-            password=os.getenv("DB_PASSWORD")
-        )
+    if db_url:
+        return psycopg2.connect(db_url, sslmode="require")
 
-    return psycopg2.connect(db_url, sslmode="require")
+    return psycopg2.connect(
+        host="localhost",
+        database="membership_db",
+        user="postgres",
+        password=os.getenv("DB_PASSWORD")
+    )
