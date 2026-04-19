8d489e0efa32e129915e282908f527c2988f12c7
ZAMBIA1 <63206519+zambia-covid@users.noreply.github.com>
2026-04-19 12:03:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,20 @@
-from flask import Flask, render_template
-import os
-
+from flask import Flask
 from auth import auth_bp, login_manager
-from agents import agents_bp
-from analytics import analytics_bp
 from members import members_bp
-from verify import verify_bp
+from analytics import analytics_bp
 
 app = Flask(__name__)
-app.secret_key = os.getenv("SECRET_KEY")
+app.secret_key = "supersecret"
 
 login_manager.init_app(app)
-login_manager.login_view = "auth.login"
 
 app.register_blueprint(auth_bp)
-app.register_blueprint(agents_bp)
-app.register_blueprint(analytics_bp)
 app.register_blueprint(members_bp)
-app.register_blueprint(verify_bp)
+app.register_blueprint(analytics_bp)
 
 @app.route("/")
-def dashboard():
-    return render_template("dashboard.html")
+def home():
+    return "System Running"
 
 if __name__ == "__main__":
     app.run(debug=True)
